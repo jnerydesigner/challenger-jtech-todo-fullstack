543e77a65998ecb55a1c0e2f4c5ae7005c7acc35
Jander da Costa Nery <jander.webmaster@gmail.com>
2026-01-14 15:40:17 -0400

    Update UserServiceTest.java

--- backend-jtech/src/test/java/com/jandernery/jtech/services/UserServiceTest.java
+++ backend-jtech/src/test/java/com/jandernery/jtech/services/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.jandernery.jtech.services;
 
+import com.jandernery.jtech.app.dtos.CreateUserDTO;
 import com.jandernery.jtech.app.services.UserService;
 import com.jandernery.jtech.domain.entities.UserEntity;
 import com.jandernery.jtech.domain.repositories.UserRepository;
@@ -11,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+import java.util.UUID;
 
 
 import static org.mockito.Mockito.*;
@@ -30,6 +32,35 @@ class UserServiceTest {
                 .thenReturn(Optional.of(mock(UserEntity.class)));
 
         userService.getByEmail("test@email.com");
+    }
+
+    @Test
+    void shouldCreateUserSuccessfully(){
+        CreateUserDTO dto = new CreateUserDTO(
+                "John Doe",
+                "john.doe@test.com",
+                "123456"
+        );
+
+        UserEntity savedUser = new UserEntity();
+        savedUser.setId(UUID.fromString("ae7847d4-8984-49db-971f-871ab3c3698c"));
+        savedUser.setName(dto.name());
+        savedUser.setEmail(dto.email());
+        savedUser.setPassword(dto.password());
+
+        when(userRepository.save(any(UserEntity.class)))
+                .thenReturn(savedUser);
+
+        UserEntity result = userService.createUser(dto);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertEquals(dto.name(), result.getName());
+        assertEquals(dto.email(), result.getEmail());
+        assertEquals(dto.password(), result.getPassword());
+
+        verify(userRepository).save(any(UserEntity.class));
+
     }
 }
 
