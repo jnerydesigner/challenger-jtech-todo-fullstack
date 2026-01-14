c010f7d973aa3eac6d157aadc44755c4cd02a60b
Jander da Costa Nery <jander.webmaster@gmail.com>
2026-01-14 14:01:41 -0400

    chore: bootstrap spring boot project with security and jpa

---
+++ backend-jtech/src/main/java/com/jandernery/jtech/JtechApplication.java
@@ -0,0 +1,13 @@
+package com.jandernery.jtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechApplication.class, args);
+	}
+
+}
